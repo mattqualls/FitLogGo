a22d0e33765079b7b9f46e0c9d74ea41d01260b7
Matt Qualls <mattqualls@comcast.net>
2016-06-20 16:15:55 -0500

    added properties to models for table with appropriate key relationships for data first

--- FitLogGo/src/FitLogGo/Models/SessionType.cs
+++ FitLogGo/src/FitLogGo/Models/SessionType.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,13 @@ namespace FitLogGo.Models
 {
     public class SessionType
     {
+        [Key]
+        public int SessionTypeId { get; set; }
+        public int SessionId { get; set; }
+        public int TreadmillId { get; set; }
+        public int BikeId { get; set; }
+        public int EllipticalId { get; set; }
+
+        public Session Session { get; set; }
     }
 }
