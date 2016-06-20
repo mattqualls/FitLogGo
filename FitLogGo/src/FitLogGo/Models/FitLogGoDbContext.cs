4f81f3cbb2ea2a2e71f1fc7fe595c37ef0cc3bb9
Matt Qualls <mattqualls@comcast.net>
2016-06-20 16:26:19 -0500

    created dbContext model and adeed ConfigureServices method info in startup.cs for connection to create and update localdb with migration

--- FitLogGo/src/FitLogGo/Models/FitLogGoDbContext.cs
+++ FitLogGo/src/FitLogGo/Models/FitLogGoDbContext.cs
@@ -2,10 +2,18 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace FitLogGo.Models
 {
-    public class FitLogGoDbContext
+    public class FitLogGoDbContext : DbContext
     {
+        public FitLogGoDbContext(DbContextOptions<FitLogGoDbContext> options)
+      : base(options)
+    { }
+
+        public DbSet<AppUser> AppUsers { get; set; }
+        public DbSet<Session> Sessions { get; set; }
+        public DbSet<SessionType> SessionTypes { get; set; }
     }
 }
