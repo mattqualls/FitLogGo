042e0744b6f82bf67b4ad2ef8cc716d2d90ee5c7
Matt Qualls <mattqualls@comcast.net>
2016-06-20 20:01:34 -0500

    added sessions controller. A functional sessions view is working displaying session date that also has a create new session link. The new session link is functional and posting new info to db

---
+++ FitLogGo/src/FitLogGo/Migrations/20160620212912_MyFirstMigration.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace FitLogGo.Migrations
+{
+    public partial class MyFirstMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "AppUsers",
+                columns: table => new
+                {
+                    AppUserId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Age = table.Column<int>(nullable: false),
+                    Email = table.Column<string>(nullable: true),
+                    InitialWeight = table.Column<int>(nullable: false),
+                    JoinDate = table.Column<DateTime>(nullable: false),
+                    Username = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AppUsers", x => x.AppUserId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Sessions",
+                columns: table => new
+                {
+                    SessionId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    AppUserId = table.Column<int>(nullable: false),
+                    CaloriesBurned = table.Column<int>(nullable: false),
+                    MilesTraveled = table.Column<double>(nullable: false),
+                    MinutesWorked = table.Column<double>(nullable: false),
+                    SessionDate = table.Column<DateTime>(nullable: false),
+                    SessionWeight = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Sessions", x => x.SessionId);
+                    table.ForeignKey(
+                        name: "FK_Sessions_AppUsers_AppUserId",
+                        column: x => x.AppUserId,
+                        principalTable: "AppUsers",
+                        principalColumn: "AppUserId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "SessionTypes",
+                columns: table => new
+                {
+                    SessionTypeId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    BikeId = table.Column<int>(nullable: false),
+                    EllipticalId = table.Column<int>(nullable: false),
+                    SessionId = table.Column<int>(nullable: false),
+                    TreadmillId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SessionTypes", x => x.SessionTypeId);
+                    table.ForeignKey(
+                        name: "FK_SessionTypes_Sessions_SessionId",
+                        column: x => x.SessionId,
+                        principalTable: "Sessions",
+                        principalColumn: "SessionId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Sessions_AppUserId",
+                table: "Sessions",
+                column: "AppUserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SessionTypes_SessionId",
+                table: "SessionTypes",
+                column: "SessionId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "SessionTypes");
+
+            migrationBuilder.DropTable(
+                name: "Sessions");
+
+            migrationBuilder.DropTable(
+                name: "AppUsers");
+        }
+    }
+}
